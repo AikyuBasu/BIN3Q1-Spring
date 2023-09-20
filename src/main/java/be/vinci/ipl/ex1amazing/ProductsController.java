178f75f3808eb07004a28cc33e7a18000925c5b0
Alicia Boltryk <alicia.boltryk@student.vinci.be>
2023-09-20 15:47:10 +0200

    doc.yaml bizarre cette histoire

--- src/main/java/be/vinci/ipl/ex1amazing/ProductsController.java
+++ src/main/java/be/vinci/ipl/ex1amazing/ProductsController.java
@@ -4,83 +4,98 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ProductsController {
-  private static final List<Product> products = new ArrayList<>();
+  private final ProductsService service;
 
-  // ajout de données bidon
-  static {
-    products.add(new Product(1, "Little spoon","Kitchenware", 6.99));
-    products.add(new Product(2, "Big bone", "Dog food woof", 59.99));
-    products.add(new Product(3, "Natural and totally legal steroids", "Bodybuilding Cheatcodes", 789.99));
+  public ProductsController(ProductsService service){
+    this.service = service;
   }
 
+  /**
+   * Create a product
+   * @request POST /products
+   * @response 201: returns the created product
+   * @response 409: if the product already exists
+   * @response 400: if the product is null
+   */
   @PostMapping("/products")
   public ResponseEntity<Product> createOne(@RequestBody Product product) {
     if (product == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    for (Product p : products) {
-      if (product.getId() == p.getId()) return new ResponseEntity<>(HttpStatus.CONFLICT);
-    }
-    products.add(product);
-    return new ResponseEntity<>(product, HttpStatus.CREATED);
+    if (service.createOne(product)) return new ResponseEntity<>(product, HttpStatus.CREATED);
+    return new ResponseEntity<>(HttpStatus.CONFLICT);
   }
 
+  /**
+   * Read all products
+   * @request GET /products
+   * @response 200: returns all videos
+   * @response 404: if there are no products
+   */
   @GetMapping("/products")
-  public Iterable<Product> readAll(){
-    return products;
-  }
+    public ResponseEntity<List<Product>> readAll(){
+        List<Product> p = service.readAll();
+        if (p.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+    }
 
+  /**
+   * Read one product
+   * @request GET /products/{id}
+   * @response 200: returns the product
+   * @response 404: if the product does not exist
+   * @response 400: if the id is invalid
+   */
   @GetMapping("/products/{id}")
   public ResponseEntity<Product> readOne(@PathVariable int id){
     if (id < 1) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    Product pFound = null;
-    for (Product p : products) {
-      if (p.getId() == id){
-        pFound = p;
-        break;
-      }
-    }
-    if (pFound == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    return new ResponseEntity<>(pFound, HttpStatus.OK);
+    Product p = service.readOne(id);
+    if (p == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(p, HttpStatus.OK);
   }
 
+
+  /**
+   * Update one product
+   * @request PUT /products/{id}
+   * @response 200: returns the updated product
+   * @response 404: if the product does not exist
+   * @response 400: if the id is invalid
+   */
   @PutMapping("/products/{id}")
   public ResponseEntity<Product> updateOne(@PathVariable int id, @RequestBody Product product){
     if (id < 1 || product == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    Product pFound = null;
-    for (Product p : products) {
-      if (p.getId() == id){
-        pFound = p;
-        break;
-      }
-    }
-    if (pFound == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    products.set(products.indexOf(pFound), product);
-    return new ResponseEntity<>(pFound, HttpStatus.OK);
+    boolean updated = service.updateOne(product);
+    if (!updated) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(product, HttpStatus.OK);
   }
 
+    /**
+     * Delete all products
+     * @request DELETE /products
+     * @response 200: returns the deleted products
+     */
   @DeleteMapping("/products")
   public ResponseEntity<Product> deleteAll(){
-    while (!products.isEmpty()) products.remove(0);
+    service.deleteAll();
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
+    /**
+     * Delete one product
+     * @request DELETE /products/{id}
+     * @response 200: returns the deleted product
+     * @response 404: if the product does not exist
+     * @response 400: if the id is invalid
+     */
   @DeleteMapping("/products/{id}")
   public ResponseEntity<Product> deleteOne(@PathVariable int id){
     if (id < 1) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    Product pFound = null;
-    for (Product p : products) {
-      if (p.getId() == id){
-        pFound = p;
-        break;
-      }
-    }
-    if (pFound == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    products.remove(pFound);
-    return new ResponseEntity<>(pFound, HttpStatus.OK);
+    Product p = service.deleteOne(id);
+    if (p == null) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    return new ResponseEntity<>(p, HttpStatus.OK);
   }
 
 
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO products(id, category, name, price) VALUES (1, 'Little spoon', 'Kitchenware', 6.99);
+INSERT INTO products(id, name, category, price) VALUES (2, 'Big bone', 'Dog food woof', 59.99);
+INSERT INTO products(id, name, category, price) VALUES (3, 'Natural and totally legal steroids', 'Bodybuilding Cheatcodes', 789.99);
